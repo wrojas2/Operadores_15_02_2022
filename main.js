29e5df61d3eeb453c2ce4b36ce54834711b998cb
wrojas2 <wiros2@gmail.com>
2022-02-15 19:56:39 -0500

    OPERADOR DE ASIGNACION_15_02_2022

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+/* OPERADOR DE ASIGNACION 
+let x;
+let y = prompt("Datos")
+let myStyle = `
+    color: #fff;
+    background: #226666;
+    padding: 5px;
+    border-radius: 5px;`;
+
+    // Asignación
+console.group(`%cOperadores Asignacion`,myStyle);
+
+    console.log (`Variable x: ${x}, Variable y: ${y}`);
+    
+    console.log (`Abreviación: x = y`)
+    // Abreviado
+    
+    x = y 
+
+    console.log (`x:${x} y: ${y}` );
+
+
+console.groupEnd(); */
+
 /* VENTANA EMERGENTE EN JS
 let nombre = prompt(`Ingrese El Nombre Del Usuario:`, `Escriba su nombre completo`);
 console.log (nombre); */
