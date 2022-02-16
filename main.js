2c2c459a11c7aa5e3416422caa8db664f350c4dd
wrojas2 <wiros2@gmail.com>
2022-02-15 21:09:27 -0500

    OPERADOR DE ADICION _15_02_2022

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+/* OPERADOR DE ADICION  
+  let a = null;
+  let b = new Number (prompt("Ingrese Datos", "Digite un dato"));
+  let myStyle1 = `
+  color: #fff;
+  background: #801515;
+  padding: 5px;
+  border-radius: 5px;`;
+  
+  // ADICION
+
+  console.group(`%cOperadores Adicion`,myStyle1);
+
+        console.log (`Variable x: ${a}, Variable y: ${b}`);
+
+    // Abreviado
+        console.log(`Abreviacion: a += b;`)
+           a += b;
+
+     // Significado
+     // console.log (`Significado: a = a + b`);
+     // x = x + y;
+     
+            console.log (`a:${a} b: ${b}` );
+
+console.groupEnd(); */
+
 /* OPERADOR DE ASIGNACION 
 let x;
 let y = prompt("Datos")
@@ -17,10 +44,10 @@ console.group(`%cOperadores Asignacion`,myStyle);
     
     x = y 
 
-    console.log (`x:${x} y: ${y}` );
+    console.log (`x:${x} y: ${y}` );*/
 
 
-console.groupEnd(); */
+console.groupEnd(); 
 
 /* VENTANA EMERGENTE EN JS
 let nombre = prompt(`Ingrese El Nombre Del Usuario:`, `Escriba su nombre completo`);
